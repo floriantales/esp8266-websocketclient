6c6850a7baceddfe18304417cbde8a621338fb00
Florian TALES <florian.tales@laposte.net>
2020-10-25 21:57:14 +0100

    Update WebsocketSample.ino

--- examples/WebsocketSample/WebsocketSample.ino
+++ examples/WebsocketSample/WebsocketSample.ino
@@ -1,11 +1,11 @@
-#include <Arduino.h>
 #include "WebSocketClient.h"
 #include "ESP8266WiFi.h"
 
-WebSocketClient ws(true);
+WebSocketClient ws(true);	// Set true for wss
 
 void setup() {
 	Serial.begin(115200);
+	Serial.println();
 	WiFi.begin("MyWifi", "secret");
 
 	Serial.print("Connecting");
@@ -13,17 +13,20 @@ void setup() {
 		delay(500);
 		Serial.print(".");
 	}
+	Serial.println();
 }
 
 void loop() {
 	if (!ws.isConnected()) {
+		Serial.println("Connecting to websocket ..");
 		ws.connect("echo.websocket.org", "/", 443);
 	} else {
+		Serial.println("Sending to websocket ..");
 		ws.send("hello");
 
 		String msg;
 		if (ws.getMessage(msg)) {
-			Serial.println(msg);
+			Serial.printf("WS Response : %s \n", msg.c_str());
 		}
 	}
 	delay(500);
